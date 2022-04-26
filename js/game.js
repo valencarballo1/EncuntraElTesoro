ef99cd9108b6027d3c4b80da36d33b9579dc9f80
valencarballo <valentincarballo99@gmail.com>
2022-04-26 11:23:08 -0300

    Simulador, Encontrar el tesoro

--- js/game.js
+++ js/game.js
@@ -1,6 +1,34 @@
 const width = 400;
 const heigh= 400;
 
+let getRandomNumber = size => {
+    return Math.floor(Math.random() * size);
+}
+
+let Distancias = (e, target) => {
+    let diffX = e.offsetX - target.x;
+    let diffY = e.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY + diffY));
+}
+
+let DistanciasDelJuego = distance =>{
+    if (distance < 30){
+        return "Estas muy cerca!"
+    } else if (distance < 40){
+        return "Estas cerca"
+    } else if (distance < 60){
+        return "caliente"
+    } else if (distance < 100){
+        return "Calido"
+    } else if (distance < 180){
+        return "Frio"
+    } else if (distance < 360){
+        return "Muy frio"
+    } else {
+        return "Congelado"
+    }
+}
+
 let target = {
     x: getRandomNumber(width),
     y: getRandomNumber(heigh)
@@ -11,8 +39,8 @@ let $distance = document.getElementById("distance");
 let clicks = 0;
 $map.addEventListener("click", function(e){
     clicks++;
-    let distance = getDistance(e, target);
-    let distanceHint = getDistanceHint(distance);
+    let distance = Distancias(e, target);
+    let distanceHint = DistanciasDelJuego(distance);
     $distance.innerHTML = `<h1>${distanceHint}</h1>`
 
     if (distance < 20){
